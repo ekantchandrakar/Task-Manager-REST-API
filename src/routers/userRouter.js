b72a80c305831b3e6b7507b3e05ff9b1879b9909
Ekant <ekantchandrakar07@gmail.com>
2021-05-25 23:43:09 +0530

    Authenticating users endpoints

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -52,21 +52,7 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every((update) => {
@@ -79,35 +65,23 @@ router.patch("/users/:id", async (req, res) => {
     })
   }
 
-  const _id = req.params.id;
   try {
-    /* const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true
-    }); */
-    const user = await User.findById(_id);
     updates.forEach((update) => {
-      user[update] = req.body[update];
+      req.user[update] = req.body[update];
     });
-    await user.save();
 
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    await req.user.remove();
+
+    res.send(req.user);
   } catch (e) {
     res.status(500).send(e);
   }
