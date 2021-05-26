01163334a4783d860566bf3b84f0d55ba03d4e17
Ekant <ekantchandrakar07@gmail.com>
2021-05-26 20:40:36 +0530

    Set-up the user and task relationship

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const Task = require("../models/task");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
-router.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
+router.post("/tasks", auth, async (req, res) => {
 
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id
+  });
   try {
     await task.save();
     res.status(201).send(task);
@@ -61,7 +65,7 @@ router.patch("/tasks/:id", async (req, res) => {
       task[update] = req.body[update];
     });
     await task.save();
-    
+
     if (!task) {
       return res.status(404).send();
     }
