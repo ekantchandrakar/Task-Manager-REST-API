463287282e719094c2a391014510691260fb218d
Ekant <ekantchandrakar07@gmail.com>
2021-05-28 18:21:26 +0530

    Set up for allowing to  sort the data

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -20,11 +20,18 @@ router.post("/tasks", auth, async (req, res) => {
 // GET /tasks?completed=true
 // limit and skip (for pagination) limit: no of result we get back, skip: allow iterate over the pages
 // GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if(req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
 
   try {
@@ -33,7 +40,8 @@ router.get("/tasks", auth, async (req, res) => {
       match,
       options: {
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort
       }
     }).execPopulate();
     // const tasks = await Task.find({ owner: req.user._id });
