8e0806d416df3deab9103ed83893b01fd4340738
Ekant <ekantchandrakar07@gmail.com>
2021-05-29 22:55:54 +0530

    Handle the express error

--- src/index.js
+++ src/index.js
@@ -18,14 +18,13 @@ const port = process.env.PORT || 3000;
 //     }
 //
 //     cb(undefined, true);
-//     // cb(new Error("File must be a PDF"));
-//     // cb(undefined, true);
-//     // cb(undefined, false);
 //   }
 // });
 //
 // app.post("/upload", upload.single("upload"), (req, res) => {
 //   res.send();
+// }, (error, req, res, next) => {
+//   res.status(400).send({ error: error.message });
 // });
 
 
